f59dd82a958230b0f588bf52860f26e09a9716e2
kangseungmin <rkdtmdals999@naver.com>
2024-02-13 16:02:07 +0900

    [feat] Customer Entity와 Repository 생성

--- src/main/java/com/example/batch/customer/Customer.java
+++ src/main/java/com/example/batch/customer/Customer.java
@@ -1,6 +1,37 @@
 package com.example.batch.customer;
 
+import com.example.batch.customer.enums.Status;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@ToString
 public class Customer {
 
+    private Long id;
 
+    private String name;
+
+    private String email;
+
+    private LocalDateTime createAt;
+
+    private LocalDateTime loginAt;
+
+    private Status status;
+
+    public Customer(String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.createAt = LocalDateTime.now();
+        this.loginAt = LocalDateTime.now();
+        this.status = Status.NORMAL;
+    }
 }
---
+++ src/main/java/com/example/batch/customer/enums/Status.java
@@ -0,0 +1,7 @@
+package com.example.batch.customer.enums;
+
+public enum Status {
+
+    NORMAL,
+    DORMANT;
+}
