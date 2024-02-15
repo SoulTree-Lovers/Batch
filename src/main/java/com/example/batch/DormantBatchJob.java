c1837da21562e35603e213557c47378bfc27deab
kangseungmin <rkdtmdals999@naver.com>
2024-02-15 16:10:37 +0900

    [feat] 배치 상태 확인 및 메시지 전송 코드 추가

--- src/main/java/com/example/batch/DormantBatchJob.java
+++ src/main/java/com/example/batch/DormantBatchJob.java
@@ -2,6 +2,8 @@ package com.example.batch;
 
 import com.example.batch.customer.Customer;
 import com.example.batch.customer.CustomerRepository;
+import com.example.batch.customer.batches.BatchStatus;
+import com.example.batch.customer.batches.JobExecution;
 import com.example.batch.customer.enums.CustomerStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Component
 public class DormantBatchJob {
@@ -22,7 +25,7 @@ public class DormantBatchJob {
         this.emailProvider = new EmailProvider.Fake();
     }
 
-    public void execute() {
+    public JobExecution execute() {
         /**
          * 1. 유저를 조회한다.
          * 2. 휴면 계정 대상을 추출 및 변환한다.
@@ -30,38 +33,56 @@ public class DormantBatchJob {
          * 4. 메일을 보낸다.
          */
 
+        final JobExecution jobExecution = new JobExecution();
+        jobExecution.setBatchStatus(BatchStatus.STARTING);
+        jobExecution.setStartTime(LocalDateTime.now());
+
         int pageNo = 0;
 
-        while (true) {
-            // 1. 유저 조회
-            final PageRequest pageRequest = PageRequest.of(pageNo, 1, Sort.by("id").ascending()); // 한 페이지씩 가져오기
-            final Page<Customer> page = customerRepository.findAll(pageRequest);
-            final Customer customer;
+        try {
+            while (true) {
+                // 1. 유저 조회
+                final PageRequest pageRequest = PageRequest.of(pageNo, 1, Sort.by("id").ascending()); // 한 페이지씩 가져오기
+                final Page<Customer> page = customerRepository.findAll(pageRequest);
+                final Customer customer;
 
-            if (page.isEmpty()) {
-                break; // 페이지에 데이터가 없다면 종료
-            } else {
-                pageNo++;
-                customer = page.getContent().get(0); // 1개씩 가져오므로 0번째 데이터를 가져온다.
+                if (page.isEmpty()) {
+                    break; // 페이지에 데이터가 없다면 종료
+                } else {
+                    pageNo++;
+                    customer = page.getContent().get(0); // 1개씩 가져오므로 0번째 데이터를 가져온다.
+                }
+
+                // 2. 휴면 계정 대상을 추출 및 변환
+                //     로그인 날짜     -      365일 전      -     오늘
+                final boolean isDormantTarget = LocalDate.now() // 오늘 날짜로부터 365일 전 날짜가, 로그인한 이후라면 휴면 계정 (오늘 - 365 > 마지막 로그인 == 휴면)
+                        .minusDays(365)
+                        .isAfter(customer.getLoginAt().toLocalDate());
+
+                if (isDormantTarget) {
+                    customer.setStatus(CustomerStatus.DORMANT);
+                } else {
+                    continue;
+                }
+
+                // 3. 휴면 계정으로 상태를 변경
+                customerRepository.save(customer);
+
+                // 4. 메일 전송
+                emailProvider.send(customer.getEmail(), "휴면 전환 안내 메일입니다.", "내용");
+
             }
-
-            // 2. 휴면 계정 대상을 추출 및 변환
-            //     로그인 날짜     -      365일 전      -     오늘
-            final boolean isDormantTarget = LocalDate.now() // 오늘 날짜로부터 365일 전 날짜가, 로그인한 이후라면 휴면 계정 (오늘 - 365 > 마지막 로그인 == 휴면)
-                    .minusDays(365)
-                    .isAfter(customer.getLoginAt().toLocalDate());
-
-            if (isDormantTarget) {
-                customer.setStatus(CustomerStatus.DORMANT);
-            } else {
-                continue;
-            }
-
-            // 3. 휴면 계정으로 상태를 변경
-            customerRepository.save(customer);
-
-            // 4. 메일 전송
-            emailProvider.send(customer.getEmail(), "휴면 전환 안내 메일입니다.", "내용");
+            jobExecution.setBatchStatus(BatchStatus.COMPLETED);
+        } catch (Exception e) {
+            jobExecution.setBatchStatus(BatchStatus.FAILED);
         }
+
+        jobExecution.setEndTime(LocalDateTime.now());
+        emailProvider.send(
+                "admin@naver.com",
+                "배치 완료 알림",
+                "DormantBatchJob이 실행되었습니다. status:" + jobExecution.getBatchStatus()
+        );
+        return jobExecution;
     }
 }
