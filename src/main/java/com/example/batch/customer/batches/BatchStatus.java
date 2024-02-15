d87cac3bcd46692a333fc7cf018a26c7ad225bb7
kangseungmin <rkdtmdals999@naver.com>
2024-02-15 16:10:11 +0900

    [feat] 배치 상태 코드 추가

--- src/main/java/com/example/batch/customer/batches/BatchStatus.java
+++ src/main/java/com/example/batch/customer/batches/BatchStatus.java
@@ -1,2 +1,14 @@
-package com.example.batch.customer.batches;public class BatchStatus {
+package com.example.batch.customer.batches;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public enum BatchStatus {
+
+    STARTING,
+    FAILED,
+    COMPLETED
+
 }
--- src/main/java/com/example/batch/customer/batches/JobExecution.java
+++ src/main/java/com/example/batch/customer/batches/JobExecution.java
@@ -1,2 +1,20 @@
-package com.example.batch.customer.batches;public class JobExecution {
+package com.example.batch.customer.batches;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@ToString
+public class JobExecution {
+
+    private BatchStatus batchStatus;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
 }
