a76ca9d069a91508483c6f519f8362151fe414e5
kangseungmin <rkdtmdals999@naver.com>
2024-02-17 18:55:28 +0900

    [feat] DormantBatchConfiguration에 preDormantBatchStep과 dormantBatchStep 메소드 구현

---
+++ src/main/java/com/example/batch/application/dormant/DormantBatchConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.batch.application.dormant;
+
+import com.example.batch.batches.Job;
+import com.example.batch.batches.Step;
+import com.example.batch.batches.StepJobBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DormantBatchConfiguration {
+
+    @Bean
+    public Job dormantBatchJob (
+            Step preDormantBatchStep,
+            Step dormantBatchStep,
+            DormantBatchJobExecutionListener JobExecutionListener
+    ) {
+        return new StepJobBuilder()
+                .start(preDormantBatchStep)
+                .next(dormantBatchStep)
+                .build();
+    }
+
+    @Bean
+    public Step preDormantBatchStep(
+            AllCustomerItemReader itemReader,
+            PreDormantBatchItemProcessor itemProcessor,
+            PreDormantBatchItemWriter itemWriter
+    ) {
+        return Step.builder()
+                .itemReader(itemReader)
+                .itemProcessor(itemProcessor)
+                .itemWriter(itemWriter)
+                .build();
+    }
+
+    @Bean
+    public Step dormantBatchStep(
+            AllCustomerItemReader itemReader,
+            DormantBatchItemProcessor itemProcessor,
+            DormantBatchItemWriter itemWriter
+    ) {
+        return Step.builder()
+                .itemReader(itemReader)
+                .itemProcessor(itemProcessor)
+                .itemWriter(itemWriter)
+                .build();
+    }
+
+
+    // TODO: 휴면 전환 예정 1주일 전인 사람에게 이메일 발송
+
+}
