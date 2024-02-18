ec7415d4cb73a29241319e6cbe3ec00a9edd6bce
kangseungmin <rkdtmdals999@naver.com>
2024-02-18 15:12:06 +0900

    [feat] Chunk를 사용하여 Commit 수 줄이기 (DB I/O)
    - tasklet 기반: 15번 commit
    - Chunk, Item 삼총사 기반: 2번 commit

--- src/main/java/com/example/batch/JobConfiguration.java
+++ src/main/java/com/example/batch/JobConfiguration.java
@@ -3,9 +3,16 @@ package com.example.batch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,20 +24,53 @@ public class JobConfiguration {
 
     @Bean
     public Job job(JobRepository jobRepository, Step step) {
-        return new JobBuilder("job", jobRepository)
+        return new JobBuilder("job-chunk", jobRepository)
                 .start(step)
                 .build();
     }
 
     @Bean
     public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-        return new StepBuilder("step", jobRepository)
-                .tasklet((a, b) -> {
-                    log.info("step 성공");
+
+        /*final Tasklet tasklet = new Tasklet() {
+            private int count = 0;
+
+            @Override
+            public RepeatStatus execute(StepContribution a, ChunkContext b) throws Exception {
+                count++;
+
+                if (count == 15) {
+                    log.info("Tasklet Finished");
                     return RepeatStatus.FINISHED;
-//                    throw new IllegalStateException("상태가 잘못되었습니다.");
-                },
-                platformTransactionManager)
+                }
+
+                log.info("Tasklet Continuable {}", count);
+                return RepeatStatus.CONTINUABLE;
+            }
+        };*/
+
+        final ItemReader<Integer> itemReader = new ItemReader<>() {
+
+            private int count = 0;
+
+            @Override
+            public Integer read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                count++;
+
+                log.info("Read: {}", count);
+                if (count == 15) {
+                    return null;
+                }
+
+                return count;
+            }
+        };
+
+        return new StepBuilder("step", jobRepository)
+                .chunk(10, platformTransactionManager)
+                .reader(itemReader)
+//                .processor()
+                .writer(read -> {})
                 .build();
     }
 }
