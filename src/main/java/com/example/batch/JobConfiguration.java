d2dd7d2059cb7ccb995269cefeae5498a5231181
kangseungmin <rkdtmdals999@naver.com>
2024-02-20 12:43:25 +0900

    [feat] StepBuilder 옵션 설정
    - skip, skipLimit
    - skipPolicy
    - noRollback
    - retry, retryLimit
    - @JobScope를 통해 jobParameter 주입

--- src/main/java/com/example/batch/JobConfiguration.java
+++ src/main/java/com/example/batch/JobConfiguration.java
@@ -4,16 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.skip.SkipLimitExceededException;
+import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -30,24 +31,24 @@ public class JobConfiguration {
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-
+    @JobScope
+    public Step step(
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager,
+            @Value("#{jobParameters['name']}") String name
+    ) {
+        log.info("name: {}", name);
         return new StepBuilder("step", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-
-                    log.info("step 실행");
+                .tasklet((a, b) -> {
                     return RepeatStatus.FINISHED;
-
                 }, platformTransactionManager)
-                .allowStartIfComplete(true) // 성공시에도 재시작 허용
-                .startLimit(5)
                 .build();
     }
 
-    /*@Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
 
-        *//*final Tasklet tasklet = new Tasklet() {
+        /*final Tasklet tasklet = new Tasklet() {
             private int count = 0;
 
             @Override
@@ -62,7 +63,7 @@ public class JobConfiguration {
                 log.info("Tasklet Continuable {}", count);
                 return RepeatStatus.CONTINUABLE;
             }
-        };*//*
+        };*/
 
         final ItemReader<Integer> itemReader = new ItemReader<>() {
 
@@ -72,20 +73,41 @@ public class JobConfiguration {
             public Integer read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
                 count++;
 
+                if (count == 20) return null;
+
                 log.info("Read: {}", count);
-                if (count == 15) {
-                    return null;
-                }
+//                if (count >= 15) {
+//                    throw new IllegalStateException("예외 발생");
+//                }
+
 
                 return count;
             }
         };
 
+        ItemProcessor<Integer, Integer> itemProcessor = new ItemProcessor<>() {
+            @Override
+            public Integer process(Integer item) throws Exception {
+                if (item == 15) throw new IllegalStateException();
+
+                return item;
+            }
+        };
+
+        final SkipPolicy skipPolicy = (t, skipCount) -> t instanceof IllegalStateException && skipCount < 5;
+
         return new StepBuilder("step", jobRepository)
-                .chunk(10, platformTransactionManager)
+                .<Integer, Integer>chunk(10, platformTransactionManager)
                 .reader(itemReader)
-//                .processor()
+                .processor(itemProcessor)
                 .writer(read -> {})
+                .faultTolerant()
+//                .skip(IllegalStateException.class) // 특정 예외에 대해서 그냥 넘어감
+//                .skipLimit(5)
+//                .skipPolicy(skipPolicy)
+//                .noRollback(IllegalStateException.class)
+                .retry(IllegalStateException.class)
+                .retryLimit(5)
                 .build();
-    }*/
+    }
 }
