fdff2dda82c4171513e5631d9718a68fbc71a467
kangseungmin <rkdtmdals999@naver.com>
2024-02-21 12:38:40 +0900

    [feat] User Model를 Entity로 지정 후 id 멤버변수 선언

--- src/main/java/com/example/batch/model/User.java
+++ src/main/java/com/example/batch/model/User.java
@@ -1,13 +1,27 @@
 package com.example.batch.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "USER")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
+
     private String age;
+
     private String region;
+
     private String phone;
 
 }
