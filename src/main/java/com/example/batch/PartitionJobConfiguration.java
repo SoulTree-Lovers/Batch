5c4fd2f0d714398461406b05331bcf46d1095e1e
kangseungmin <rkdtmdals999@naver.com>
2024-02-21 15:53:38 +0900

    [feat] Partitioning 기반 Step 및 Partitioner 구현

---
+++ src/main/java/com/example/batch/PartitionJobConfiguration.java
@@ -0,0 +1,105 @@
+package com.example.batch;
+
+import com.example.batch.model.User;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.partition.PartitionHandler;
+import org.springframework.batch.core.partition.support.TaskExecutorPartitionHandler;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+public class PartitionJobConfiguration {
+
+    @Bean
+    public Job job(
+            JobRepository jobRepository,
+            Step managerStep
+    ) {
+        return new JobBuilder("partitionJobConfiguration", jobRepository)
+                .start(managerStep)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step managerStep(
+            JobRepository jobRepository,
+            Step step,
+            PartitionHandler partitionHandler,
+            DataSource dataSource
+    ) {
+        return new StepBuilder("managerStep", jobRepository)
+                .partitioner("delegateStep", new ColumnRangePartitioner(dataSource))
+                .step(step)
+                .partitionHandler(partitionHandler)
+                .build();
+    }
+
+    @Bean
+    public PartitionHandler partitionHandler(
+            Step step
+    ) {
+        final TaskExecutorPartitionHandler taskExecutorPartitionHandler = new TaskExecutorPartitionHandler();
+        taskExecutorPartitionHandler.setStep(step);
+        taskExecutorPartitionHandler.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        taskExecutorPartitionHandler.setGridSize(5);
+        return taskExecutorPartitionHandler;
+    }
+
+    @Bean
+    public Step step(
+            JobRepository jobRepository,
+            JpaPagingItemReader<User> jpaPagingItemReader,
+            PlatformTransactionManager platformTransactionManager
+    ) {
+        return new StepBuilder("step", jobRepository)
+                .<User, User>chunk(4, platformTransactionManager)
+                .reader(jpaPagingItemReader)
+                .writer(result -> log.info(result.toString()))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<User> jpaPagingItemReader(
+            @Value("#{stepExecutionContext[minValue]}") Long minValue,
+            @Value("#{stepExecutionContext[maxValue]}") Long maxValue,
+            EntityManagerFactory entityManagerFactory
+    ) {
+        log.info("minValue: {}", minValue);
+        log.info("maxValue: {}", maxValue);
+
+        final Map<String, Object> params = new HashMap<>();
+        params.put("minValue", minValue);
+        params.put("maxValue", maxValue);
+
+        return new JpaPagingItemReaderBuilder<User>()
+                .name("jpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(5)
+                .queryString("""
+                    select u from User u
+                    where u.id between :minValue and :maxValue
+                """)
+                .parameterValues(params)
+                .build();
+    }
+}
