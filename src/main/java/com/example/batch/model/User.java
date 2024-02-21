6da7cd5b245e2b7f9ad6ca13cc95068ac68fdc57
kangseungmin <rkdtmdals999@naver.com>
2024-02-21 12:16:17 +0900

    [feat] User Data Model class 생성

---
+++ src/main/java/com/example/batch/model/User.java
@@ -0,0 +1,13 @@
+package com.example.batch.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+    private String age;
+    private String region;
+    private String phone;
+
+}
