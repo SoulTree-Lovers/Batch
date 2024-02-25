5ac4d4b1876981bcc73bf9c8484d4794b9340907
kangseungmin <rkdtmdals999@naver.com>
2024-02-25 18:07:05 +0900

    [feat] SettleGroup DB Writer 구현

---
+++ src/main/java/com/example/batch/group/SettleGroupItemDbWriter.java
@@ -0,0 +1,27 @@
+package com.example.batch.group;
+
+import com.example.batch.domain.SettleGroup;
+import com.example.batch.domain.repository.SettleGroupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SettleGroupItemDbWriter implements ItemWriter<List<SettleGroup>> {
+
+    private final SettleGroupRepository settleGroupRepository;
+
+    @Override
+    public void write(Chunk<? extends List<SettleGroup>> chunk) throws Exception {
+        final List<SettleGroup> settleGroups = new ArrayList<>();
+
+        chunk.forEach(settleGroups::addAll);
+
+        settleGroupRepository.saveAll(settleGroups);
+    }
+}
